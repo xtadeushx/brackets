b775aed972c869a47152103298079715bfbb5463
Oleksandr Cherkasov <alekscherkasov13@gmail.com>
2021-07-30 19:35:28 +0300

    fill: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(text) {
+  var i, l = text.length, char, last, stack = [];
+
+  for (i = 0; i < l; i++) {
+    char = text[i];
+    if (char == "{" || char == "(" || char == "[") {
+      stack.push(char);
+      last = char;
+    } else if (char == '}' || char == ")" || char == "]") {
+      if (last) {
+        if ((char == '}' && last == "{") || (char == ')' && last == '(') || (char == ']' && last == '[')) {
+          stack.pop();
+          last = stack.length > 0 ? stack[stack.length - 1] : undefined;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;
 }
